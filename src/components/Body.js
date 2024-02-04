8bfdb3fb487829f79421286c437a527ac4b7bcb2
deepjoy9 <deepjoysarkar9@gmail.com>
2024-02-04 23:11:48 +0530

    changes commit

---
+++ src/components/Body.js
@@ -0,0 +1,116 @@
+import RestaurantCard, { withPromtedLabel } from "./RestaurantCard";
+import { useState, useEffect, useContext } from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const RestaurantCardPromoted = withPromtedLabel(RestaurantCard);
+  const onlineStatus = useOnlineStatus();
+  const { loggedInUser, setUserName } = useContext(UserContext);
+
+  const fetchData = async () => {
+    const data = await fetch(API_URL);
+    const jsonD = await data.json();
+    // Optional Chaining
+    const data1 =
+      jsonD?.data?.success?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setListOfRestaurants(data1);
+    setFilteredList(data1);
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    const json = await data.json();
+
+    setListOfRestraunt(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you're offline!! Please check your internet connection;
+      </h1>
+    );
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
+          <input
+            type="text"
+            data-testid="searchInput"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              // Filter the restraunt cards and update the UI
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="search m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setFilteredRestaurant(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+        <div className="search m-4 p-4 flex items-center">
+          <label>UserName : </label>
+          <input
+            className="border border-black p-2"
+            value={loggedInUser}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap">
+        {filteredRestaurant.map((restaurant) => (
+          <Link
+            key={restaurant?.info.id}
+            to={"/restaurants/" + restaurant?.info.id}
+          >
+            {restaurant?.info.promoted ? (
+              <RestaurantCardPromoted resData={restaurant?.info} />
+            ) : (
+              <RestaurantCard resData={restaurant?.info} />
+            )}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Shimmer.js
@@ -0,0 +1,22 @@
+const Shimmer = () => {
+    return (
+      <div className="shimmer-container">
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+        <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+      </div>
+    );
+  };
+  
+  export default Shimmer;
