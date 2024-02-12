4f78ebd36da37094b01d9bc1ab20dfec7aedffaf
deepjoy9 <deepjoysarkar9@gmail.com>
2024-02-13 00:14:54 +0530

    Refactor Shimmer component to use map for generating shimmer boxes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,25 +1,26 @@
 import React from "react";
 
+function ShimmerBox({ key }) {
+  return (
+    <div
+      key={key}
+      className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"
+    ></div>
+  );
+}
+
 function Shimmer() {
+  const shimmerBoxCount = 20;
+  const shimmerBoxes = Array.from({ length: shimmerBoxCount }, (_, index) => (
+    <ShimmerBox key={index} />
+  ));
+
   return (
     <div data-testid="shimmer">
       <div className="h-40 bg-slate-200"></div>
       <div className="mx-10 my-10 p-5">
         <div className="res-container grid grid-cols-5 mx-4 gap-12">
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
-          <div className="shadow-lg border bg-slate-200 rounded-lg h-[250px]"></div>
+          {shimmerBoxes}
         </div>
       </div>
     </div>
